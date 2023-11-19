49859c58e6cf84fcab1e775471211749982335f7
Alan <alan2000129@gmail.com>
2023-11-19 17:04:08 -0500

    Add assets and layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
-import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title:'LifePuzzle',
+  description:'Share your story with your buddies.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
 
 export default function RootLayout({
   children,
@@ -7,10 +31,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover: text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
- 
+        <body className={`${inter.variable} ${SpaceGrotesk}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
