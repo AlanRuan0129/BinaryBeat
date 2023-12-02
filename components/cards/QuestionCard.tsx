ace830d2f79554b02c8f63f56975ebe966b3d025
Alan <alanrisingpeng@gmail.com>
2023-12-01 20:24:19 -0500

    Add api/webhook to public routes
    Co-authored-by: Shuqi Liang <cheskaqiqi@gmail.com>

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -15,17 +15,14 @@ interface QuestionProps {
     _id: string;
     name: string;
     picture: string;
-    clerkId: string;
   };
-  upvotes: string[];
+  upvotes: number;
   views: number;
   answers: Array<object>;
   createdAt: Date;
-  clerkId?: string | null;
 }
 
 const QuestionCard = ({
-  clerkId,
   _id,
   title,
   tags,
@@ -68,29 +65,28 @@ const QuestionCard = ({
           isAuthor
           textStyles="body-medium text-dark400_light700"
         />
-        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
-          <Metric
-            imgUrl="/assets/icons/like.svg"
-            alt="Upvotes"
-            value={formatAndDivideNumber(upvotes.length)}
-            title=" Votes"
-            textStyles="small-medium text-dark400_light800"
-          />
-          <Metric
-            imgUrl="/assets/icons/message.svg"
-            alt="message"
-            value={formatAndDivideNumber(answers.length)}
-            title=" Answers"
-            textStyles="small-medium text-dark400_light800"
-          />
-          <Metric
-            imgUrl="/assets/icons/eye.svg"
-            alt="eye"
-            value={formatAndDivideNumber(views)}
-            title=" Views"
-            textStyles="small-medium text-dark400_light800"
-          />
-        </div>
+
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
       </div>
     </div>
   );
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -77,7 +77,9 @@ export async function deleteUser(params: DeleteUserParams) {
     const userQuestionIds = await Question.find({
       author: user._id,
     }).distinct('_id');
+
     await Question.deleteMany({ author: user._id });
+
     const deletedUser = await User.findByIdAndDelete(user._id);
 
     return deletedUser;
