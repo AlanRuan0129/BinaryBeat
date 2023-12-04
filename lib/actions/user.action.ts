3bac6603a84326679041e738b06d0fae8b319bb0
Alan <alanrisingpeng@gmail.com>
2023-12-03 23:44:22 -0500

    Display all saved questions
    Co-authored-by: Shuqi Liang <cheskaqiqi@gmail.com>

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,16 +1,19 @@
 'use server';
 
+import { FilterQuery } from 'mongoose';
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
 import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from './shared.types';
 import { revalidatePath } from 'next/cache';
 import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
 
 export async function getUserById(params: any) {
   try {
@@ -75,9 +78,11 @@ export async function deleteUser(params: DeleteUserParams) {
     // get user question ids
     // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
 
+    // delete user questions
     // @ts-ignore
     await Question.deleteMany({ author: user._id });
 
+    // TODO: delete user answers, comments, etc.
     // @ts-ignore
     const deletedUser = await User.findByIdAndDelete(user._id);
 
@@ -136,6 +141,56 @@ export async function toggleSaveQuestion(
     }
 
     revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getSavedQuestions(
+  params: GetSavedQuestionsParams
+) {
+  try {
+    connectToDatabase();
+
+    const {
+      clerkId,
+      // eslint-disable-next-line no-unused-vars
+      page = 1,
+      // eslint-disable-next-line no-unused-vars
+      pageSize = 10,
+      // eslint-disable-next-line no-unused-vars
+      filter,
+      searchQuery,
+    } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+      : {};
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: 'saved',
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        {
+          path: 'author',
+          model: User,
+          select: '_id clerkId name picture',
+        },
+      ],
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const savedQuestions = user.saved;
+
+    return { questions: savedQuestions };
   } catch (error) {
     console.log(error);
     throw error;
