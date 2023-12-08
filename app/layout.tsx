e9443a8f5525e809621f0254f9cf586b1bd03654
Alan <alanrisingpeng@gmail.com>
2023-12-08 13:14:23 -0500

    Fix layout error
    Co-authored-by: Shuqi Liang <cheskaqiqi@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter, Space_Grotesk } from 'next/font/google';
 import type { Metadata } from 'next';
 import './globals.css';
 import '../styles/prism.css';
-import { ThemeProvider } from '@/context/ThemeProvider';
+// import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -55,7 +55,7 @@ export default function RootLayout({
             },
           }}
         >
-          <ThemeProvider>{children}</ThemeProvider>
+          {/* <ThemeProvider>{children}</ThemeProvider> */}
         </ClerkProvider>
       </body>
     </html>
